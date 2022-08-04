44c5224f02cfb5938b3462020a11a241fb268368
Akın Aksoy <akinaksoytr@gmail.com>
2022-08-04 21:26:59 +0300

    Initial commit

---
+++ MyMovieList/Scenes/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieList
+//
+//  Created by Akın Aksoy on 4.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
